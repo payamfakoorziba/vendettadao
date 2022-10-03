4601a18fe58f11e845ac51e153492801f1599f5b
Payam Fakoorziba <payam.fakoorziba@mail.utoronto.ca>
2022-10-03 17:51:38 -0400

    commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Button = () => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700
+     text-white font-bold py-2 px-4 rounded mt-2">
+      Button
+    </button>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+import { Button } from "components/Button";
+import { Example } from "components/Example";
+import Navbar from "components/Navbar";
+
 export default function Home() {
   return (
     <>
+      <Navbar />
+      {/* <Example /> */}
       <main className="container min-h-screen grid pt-48 md:pt-0 md:place-items-center">
+        
         <div className="md:text-center">
           <h1 className="text-5xl font-semibold ">
-            Next.js, TailwindCSS, TypeScript Template!
+            Vendetta DAO
           </h1>
+          <Button />
           <p className="text-2xl text-gray-600 md:max-w-2xl mx-auto mt-12 md:mt-6">
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda,
             architecto, ducimus cumque temporibus optio quae dolore expedita
