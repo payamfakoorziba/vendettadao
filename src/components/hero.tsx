f318c29f05cab5490874460c71f68033adb641cc
parssak <parssak@gmail.com>
2022-11-24 12:34:18 -0500

    feat: fix deployment

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,6 +9,7 @@ export const Hero = ({ children }: { children: React.ReactNode }) => (
       </video>
       <div className="absolute inset-0 bg-black/80"></div>
       <div className="absolute inset-0 flex items-center justify-center flex-col">
+        {/*  */}
         {/* <h1 className="text-6xl text-white font-semibold">VENDETTA DAO</h1>
         <h3 className="text-3xl text-white">learn more</h3> */}
         {children}
